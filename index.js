2825edbb0254cd1a9bef1753d5977723b4e23813
Nitesh000 <ntudu040@gmail.com>
2023-03-10 12:24:27 +0530

    feat(new): new

--- index.js
+++ index.js
@@ -12,11 +12,27 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// adding routes
+
 app.post("/api/search", async (req, res) => {
   const search = req.body.search;
   const data = await Zor.zoroSearch(`${search}`);
   res.send(data);
 });
+
+app.get("/gogo/search/:animeName", async (req, res) => {
+  const animeName = req.params.animeName;
+  try {
+    const data = await fetch(
+      `https://api.consumet.org/anime/gogoanime/${animeName}?page=1`
+    );
+    const json = await data.json();
+    res.send(json);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
 app.get("/name/:name", (req, res) => {
   res.send("Hello " + req.params.name + " your age is " + req.query.age);
 });
